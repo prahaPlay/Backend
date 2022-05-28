7261cd3fd1b82c3b10a6ab5388fdaf2152024191
SWL <SWL@DESKTOP-VLCPVK4>
2022-05-28 15:44:56 +0900

    ㄴㄴㄴㄴㄴ

--- test/src/test/adadsad.java
+++ test/src/test/adadsad.java
@@ -2,10 +2,7 @@ package test;
 
 public class adadsad {
 	public static void main(String[] args) {
-		System.out.println("asdasdasdad");
-<<<<<<< sangwon
-		System.out.println("as123123das");
-=======
->>>>>>> 5310eb0 asdasdasd
+		System.out.println("변경후");
+
 	}
 }
